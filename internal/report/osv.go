65d90b39b6bb05533ab9e29f7a5a67a1833a876b
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-04-03 01:49:37 +0700

    update

---
+++ internal/report/osv.go
@@ -0,0 +1,171 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package report
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+	"time"
+
+	"golang.org/x/exp/maps"
+	"golang.org/x/vulndb/internal/derrors"
+	"golang.org/x/vulndb/internal/osv"
+	"golang.org/x/vulndb/internal/stdlib"
+)
+
+var (
+	// osvDir is the name of the directory in the vulndb repo that
+	// contains reports.
+	OSVDir = "data/osv"
+
+	// schemaVersion is used to indicate which version of the OSV schema a
+	// particular vulnerability was exported with.
+	schemaVersion = "1.3.1"
+)
+
+// GenerateOSVEntry create an osv.Entry for a report.
+// In addition to the report, it takes the ID for the vuln and the time
+// the vuln was last modified.
+func (r *Report) GenerateOSVEntry(goID string, lastModified time.Time) osv.Entry {
+	var credits []osv.Credit
+	if r.Credit != "" {
+		credits = append(credits, osv.Credit{
+			Name: r.Credit,
+		})
+	}
+
+	var withdrawn *osv.Time
+	if r.Withdrawn != nil {
+		withdrawn = &osv.Time{Time: *r.Withdrawn}
+	}
+
+	entry := osv.Entry{
+		ID:            goID,
+		Published:     osv.Time{Time: r.Published},
+		Modified:      osv.Time{Time: lastModified},
+		Withdrawn:     withdrawn,
+		Details:       trimWhitespace(r.Description),
+		Credits:       credits,
+		SchemaVersion: schemaVersion,
+	}
+
+	linkName := GetGoAdvisoryLink(goID)
+	for _, m := range r.Modules {
+		entry.Affected = append(entry.Affected, generateAffected(m, linkName))
+	}
+	for _, ref := range r.References {
+		entry.References = append(entry.References, osv.Reference{
+			Type: ref.Type,
+			URL:  ref.URL,
+		})
+	}
+	entry.Aliases = r.GetAliases()
+	return entry
+}
+
+func GetOSVFilename(goID string) string {
+	return filepath.Join(OSVDir, goID+".json")
+}
+
+// ReadOSV reads an osv.Entry from a file.
+func ReadOSV(filename string) (entry osv.Entry, err error) {
+	derrors.Wrap(&err, "ReadOSV(%s)", filename)
+	if err = UnmarshalFromFile(filename, &entry); err != nil {
+		return osv.Entry{}, err
+	}
+	return entry, nil
+}
+
+func UnmarshalFromFile(path string, v any) (err error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(content, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ModulesForEntry returns the list of modules affected by an OSV entry.
+func ModulesForEntry(entry osv.Entry) []string {
+	mods := map[string]bool{}
+	for _, a := range entry.Affected {
+		mods[a.Module.Path] = true
+	}
+	return maps.Keys(mods)
+}
+
+func AffectedRanges(versions []VersionRange) []osv.Range {
+	a := osv.Range{Type: osv.RangeTypeSemver}
+	if len(versions) == 0 || versions[0].Introduced == "" {
+		a.Events = append(a.Events, osv.RangeEvent{Introduced: "0"})
+	}
+	for _, v := range versions {
+		if v.Introduced != "" {
+			a.Events = append(a.Events, osv.RangeEvent{Introduced: v.Introduced.Canonical()})
+		}
+		if v.Fixed != "" {
+			a.Events = append(a.Events, osv.RangeEvent{Fixed: v.Fixed.Canonical()})
+		}
+	}
+	return []osv.Range{a}
+}
+
+// trimWhitespace removes unnecessary whitespace from a string, but preserves
+// paragraph breaks (indicated by two newlines).
+func trimWhitespace(s string) string {
+	s = strings.TrimSpace(s)
+	// Replace single newlines with spaces.
+	newlines := regexp.MustCompile(`([^\n])\n([^\n])`)
+	s = newlines.ReplaceAllString(s, "$1 $2")
+	// Replace instances of 2 or more newlines with exactly two newlines.
+	paragraphs := regexp.MustCompile(`\s*\n\n\s*`)
+	s = paragraphs.ReplaceAllString(s, "\n\n")
+	// Replace tabs and double spaces with single spaces.
+	spaces := regexp.MustCompile(`[ \t]+`)
+	s = spaces.ReplaceAllString(s, " ")
+	return s
+}
+
+func generateImports(m *Module) (imps []osv.Package) {
+	for _, p := range m.Packages {
+		syms := append([]string{}, p.Symbols...)
+		syms = append(syms, p.DerivedSymbols...)
+		sort.Strings(syms)
+		imps = append(imps, osv.Package{
+			Path:    p.Package,
+			GOOS:    p.GOOS,
+			GOARCH:  p.GOARCH,
+			Symbols: syms,
+		})
+	}
+	return imps
+}
+
+func generateAffected(m *Module, url string) osv.Affected {
+	name := m.Module
+	switch name {
+	case stdlib.ModulePath:
+		name = osv.GoStdModulePath
+	case stdlib.ToolchainModulePath:
+		name = osv.GoCmdModulePath
+	}
+	return osv.Affected{
+		Module: osv.Module{
+			Path:      name,
+			Ecosystem: osv.GoEcosystem,
+		},
+		Ranges:           AffectedRanges(m.Versions),
+		DatabaseSpecific: osv.DatabaseSpecific{URL: url},
+		EcosystemSpecific: &osv.EcosystemSpecific{
+			Packages: generateImports(m),
+		},
+	}
+}
---
+++ internal/report/osv_test.go
@@ -0,0 +1,252 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package report
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/vulndb/internal/osv"
+)
+
+func TestGenerateOSVEntry(t *testing.T) {
+	r := &Report{
+		Modules: []*Module{
+			{
+				Module: "example.com/vulnerable/v2",
+				Versions: []VersionRange{
+					{Fixed: "2.1.1"},
+					{Introduced: "2.3.4", Fixed: "2.3.5"},
+					{Introduced: "2.5.0"},
+				},
+				Packages: []*Package{
+					{
+						Package:        "example.com/vulnerable/v2",
+						GOOS:           []string{"windows"},
+						GOARCH:         []string{"arm64"},
+						Symbols:        []string{"A", "B.b"},
+						DerivedSymbols: []string{"D"},
+					},
+				},
+			}, {
+				Module: "vanity.host/vulnerable",
+				Versions: []VersionRange{
+					{Fixed: "2.1.1"},
+					{Introduced: "2.3.4", Fixed: "2.3.5"},
+					{Introduced: "2.5.0"},
+				},
+				Packages: []*Package{
+					{
+						Package: "vanity.host/vulnerable/package",
+						GOOS:    []string{"windows"},
+						GOARCH:  []string{"arm64"},
+						Symbols: []string{"A.b", "b"},
+					},
+				},
+			}, {
+				Module: "example.com/also-vulnerable",
+				Versions: []VersionRange{
+					{Fixed: "2.1.1"},
+				},
+				Packages: []*Package{
+					{
+						Package: "example.com/also-vulnerable/package",
+						GOOS:    []string{"windows"},
+						GOARCH:  []string{"arm64"},
+						Symbols: []string{"z"},
+					},
+				},
+			},
+		},
+		Description: "It's a real bad one, \nI'll tell you that.\n\n More info.\n",
+		CVEs:        []string{"CVE-0000-0000"},
+		GHSAs:       []string{"GHSA-abcd-efgh"},
+		Credit:      "gopherbot",
+		References: []*Reference{
+			{Type: osv.ReferenceTypeAdvisory, URL: "advisory"},
+			{Type: osv.ReferenceTypeReport, URL: "issue"},
+			{Type: osv.ReferenceTypeFix, URL: "fix"},
+			{Type: osv.ReferenceTypeWeb, URL: "web"},
+		},
+	}
+
+	wantEntry := osv.Entry{
+		SchemaVersion: schemaVersion,
+		ID:            "GO-1991-0001",
+		Details:       "It's a real bad one, I'll tell you that.\n\nMore info.",
+		References: []osv.Reference{
+			{Type: "ADVISORY", URL: "advisory"},
+			{Type: "REPORT", URL: "issue"},
+			{Type: "FIX", URL: "fix"},
+			{Type: "WEB", URL: "web"},
+		},
+		Aliases: []string{"CVE-0000-0000", "GHSA-abcd-efgh"},
+		Affected: []osv.Affected{
+			{
+				Module: osv.Module{
+					Path:      "example.com/vulnerable/v2",
+					Ecosystem: "Go",
+				},
+				Ranges: []osv.Range{
+					{
+						Type: osv.RangeTypeSemver,
+						Events: []osv.RangeEvent{
+							{
+								Introduced: "0",
+							},
+							{
+								Fixed: "2.1.1",
+							},
+							{
+								Introduced: "2.3.4",
+							},
+							{
+								Fixed: "2.3.5",
+							},
+							{
+								Introduced: "2.5.0",
+							},
+						},
+					},
+				},
+				DatabaseSpecific: osv.DatabaseSpecific{URL: "https://pkg.go.dev/vuln/GO-1991-0001"},
+				EcosystemSpecific: &osv.EcosystemSpecific{
+					Packages: []osv.Package{
+						{
+							Path:    "example.com/vulnerable/v2",
+							GOOS:    []string{"windows"},
+							GOARCH:  []string{"arm64"},
+							Symbols: []string{"A", "B.b", "D"},
+						},
+					},
+				},
+			},
+			{
+				Module: osv.Module{
+					Path:      "vanity.host/vulnerable",
+					Ecosystem: "Go",
+				},
+				Ranges: []osv.Range{
+					{
+						Type: osv.RangeTypeSemver,
+						Events: []osv.RangeEvent{
+							{
+								Introduced: "0",
+							},
+							{
+								Fixed: "2.1.1",
+							},
+							{
+								Introduced: "2.3.4",
+							},
+							{
+								Fixed: "2.3.5",
+							},
+							{
+								Introduced: "2.5.0",
+							},
+						},
+					},
+				},
+				DatabaseSpecific: osv.DatabaseSpecific{URL: "https://pkg.go.dev/vuln/GO-1991-0001"},
+				EcosystemSpecific: &osv.EcosystemSpecific{
+					Packages: []osv.Package{
+						{
+							Path:    "vanity.host/vulnerable/package",
+							GOOS:    []string{"windows"},
+							GOARCH:  []string{"arm64"},
+							Symbols: []string{"A.b", "b"},
+						},
+					},
+				},
+			},
+			{
+				Module: osv.Module{
+					Path:      "example.com/also-vulnerable",
+					Ecosystem: "Go",
+				},
+				Ranges: []osv.Range{
+					{
+						Type: osv.RangeTypeSemver,
+						Events: []osv.RangeEvent{
+							{
+								Introduced: "0",
+							},
+							{
+								Fixed: "2.1.1",
+							},
+						},
+					},
+				},
+				DatabaseSpecific: osv.DatabaseSpecific{URL: "https://pkg.go.dev/vuln/GO-1991-0001"},
+				EcosystemSpecific: &osv.EcosystemSpecific{
+					Packages: []osv.Package{
+						{
+							Path:    "example.com/also-vulnerable/package",
+							GOOS:    []string{"windows"},
+							GOARCH:  []string{"arm64"},
+							Symbols: []string{"z"},
+						},
+					},
+				},
+			},
+		},
+		Credits: []osv.Credit{
+			{
+				Name: "gopherbot",
+			},
+		},
+	}
+
+	gotEntry := r.GenerateOSVEntry("GO-1991-0001", time.Time{})
+	if diff := cmp.Diff(wantEntry, gotEntry, cmp.Comparer(func(a, b time.Time) bool { return a.Equal(b) })); diff != "" {
+		t.Errorf("GenerateOSVEntry returned unexpected entry (-want +got):\n%s", diff)
+	}
+}
+
+func TestGetOSVFilename(t *testing.T) {
+	want := "data/osv/GO-1999-0001.json"
+	if got := GetOSVFilename("GO-1999-0001"); got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestSemverCanonicalize(t *testing.T) {
+	in := []VersionRange{
+		{
+			Introduced: "1.16.0",
+			Fixed:      "1.17.0",
+		},
+	}
+	expected := []osv.Range{
+		{
+			Type: osv.RangeTypeSemver,
+			Events: []osv.RangeEvent{
+				{
+					Introduced: "1.16.0",
+				},
+				{
+					Fixed: "1.17.0",
+				},
+			},
+		},
+	}
+
+	out := AffectedRanges(in)
+	if !reflect.DeepEqual(out, expected) {
+		t.Fatalf("unexpected output: got %#v, want %#v", out, expected)
+	}
+}
+
+func TestTrimWhitespace(t *testing.T) {
+	s := "\n Lorem ipsum dolor sit amet,\n consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n \tDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \n\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n"
+	want := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	got := trimWhitespace(s)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("removeNewlines() mismatch (-want, +got):\n%s", diff)
+	}
+}
